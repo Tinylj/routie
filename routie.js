33df7dd636b3a6559ad57d9c208065eeee14467e
lujin <luj@lhcis.com>
2018-06-01 16:44:12 +0800

    路由代码提交

--- routie.js
+++ routie.js
@@ -1,26 +1,40 @@
+
+Array.prototype.getbyfield = function(field,value)
+{
+    var _this = this;
+    var rlist =[];
+    for(var i =0;i<_this.length;i++)
+    {
+        if(_this[i][field] == value)
+        {
+            rlist.push(_this[i]);
+        }
+    }
+    return rlist;
+}
 var routie = function(InitParam)
 {
     //增加一个getObjById的方法，吼吼你懂得，不可以嚣张，知道吗
 
-    this.currentUrl = location.hash.slice(1);
+    this.currentUrl = "";
     var _this = this;
     window.addEventListener('hashchange',function()
     {
-        _this.loadHash();
+        debugger;
+        _this.currentUrl = location.hash.slice(1);
+        var myobj = _this.UrlList.getbyfield("url",_this.currentUrl)[0];
+        myobj.func();
     });
-    this.loadHash = function()
-    {
-
-    };
     this.UrlList = [];
     //传参一个url 和 一个 函数
-    this.state = function(url,funcObj)
+    this.state = function(url,func)
     {
-        this.UrlList.push(
+        _this.UrlList.push(
             {
                 url:url,
-                fobj:funcObj
+                func:func
             });
+        return _this;
     };
     // 处理404 啥的逻辑，你懂吗？啊？你啥也不懂，所以你哔哔啥
     this.OtherWiseList = [];
